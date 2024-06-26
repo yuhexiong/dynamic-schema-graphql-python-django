5c04360d51433eaf9cee0e00a51c6dac2141155c
yuhexiong <yuhexiong871210@gmail.com>
2024-06-26 09:49:32 +0800

    feat: costum filter

---
+++ myapp/filters.py
@@ -0,0 +1,49 @@
+import graphene
+from django.db import models
+
+
+class FilterBetweenInput(graphene.InputObjectType):
+    start = graphene.String()
+    end = graphene.String()
+
+class FilterIntInput(graphene.InputObjectType):
+    equals = graphene.Int()
+    greater_than = graphene.Int()
+    greater_than_or_equals = graphene.Int()
+    less_than = graphene.Int()
+    less_than_or_equals = graphene.Int()
+    between = graphene.Field(FilterBetweenInput)
+
+class FilterFloatInput(graphene.InputObjectType):
+    equals = graphene.Float()
+    greater_than = graphene.Float()
+    greater_than_or_equals = graphene.Float()
+    less_than = graphene.Float()
+    less_than_or_equals = graphene.Float()
+    between = graphene.Field(FilterBetweenInput)
+
+class FilterStringInput(graphene.InputObjectType):
+    equals = graphene.String()
+    contains = graphene.List(graphene.String)
+    greater_than = graphene.String()
+    greater_than_or_equals = graphene.String()
+    less_than = graphene.String()
+    less_than_or_equals = graphene.String()
+    between = graphene.Field(FilterBetweenInput)
+
+class FilterDateInput(graphene.InputObjectType):
+    equals = graphene.Date()
+    greater_than = graphene.Date()
+    less_than = graphene.Date()
+    between = graphene.Field(FilterBetweenInput)
+
+
+def get_filter_argument(field):
+    if isinstance(field, models.IntegerField):
+        return {field.name: graphene.Argument(FilterIntInput)}
+    elif isinstance(field, models.FloatField):
+        return {field.name: graphene.Argument(FilterFloatInput)}
+    elif isinstance(field, models.DateField):
+        return {field.name: graphene.Argument(FilterDateInput)}
+    else:
+        return {field.name: graphene.Argument(FilterStringInput)}
--- myapp/schema.py
+++ myapp/schema.py
@@ -1,26 +1,45 @@
 import graphene
 from myapp import models
+from myapp.filters import get_filter_argument
+from django.db.models import Q 
 
 class Query(graphene.ObjectType):
     pass
 
-
 for table_name, graphql_type in models.graphql_types.items():
     def make_resolver(graphql_type):
         def resolve_dynamic_model(self, info, **kwargs):
-            filters = {key: value for key, value in kwargs.items() if value is not None}
-            return graphql_type._meta.model.objects.filter(**filters)
+            query = Q()
+            for field, filters in kwargs.items():
+                if filters:
+                    for operation, value in filters.items():
+                        if value is not None:
+                            if operation == 'between':
+                                query &= Q(**{f"{field}__range": (value['start'], value['end'])})
+                            elif operation == 'greater_than':
+                                query &= Q(**{f"{field}__gt": value})
+                            elif operation == 'greater_than_or_equals':
+                                query &= Q(**{f"{field}__gte": value})
+                            elif operation == 'less_than':
+                                query &= Q(**{f"{field}__lt": value})
+                            elif operation == 'less_than_or_equals':
+                                query &= Q(**{f"{field}__lte": value})
+                            elif operation == 'contains':
+                                query &= Q(**{f"{field}__in": value})
+                            elif operation == 'equals':
+                                query &= Q(**{f"{field}": value})
+                            else:
+                                query_key = f"{field}__{operation}"
+                                query &= Q(**{query_key: value})
+            return graphql_type._meta.model.objects.filter(query)
         return resolve_dynamic_model
 
-    args = {
-        field.name: graphene.String() for field in graphql_type._meta.model._meta.fields
-    }
+    args = {}
+    for field in graphql_type._meta.model._meta.fields:
+        args.update(get_filter_argument(field))
 
-    resolve_func = make_resolver(graphql_type)
     field_name = table_name.lower()
-
-    Query._meta.fields[field_name] = graphene.Field(graphene.List(graphql_type), resolver=resolve_func, args=args)
+    Query._meta.fields[field_name] = graphene.Field(graphene.List(graphql_type), resolver=make_resolver(graphql_type), args=args)
     print(f"Added field '{field_name}' to Query with type {graphql_type}")
-
 
 schema = graphene.Schema(query=Query, types=list(models.graphql_types.values()))
